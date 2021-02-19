9a0b07f3b822fff2b92e275233e4d978b2faf3fd
Juergen Schwind <info@jbs-newmedia.de>
2021-02-19 13:43:36 +0100

    Adminer von 4.7.9 auf 4.8.0 aktualisiert

--- source/tools.adminer/stable/oswtools/tools.adminer.stable/adminer.php
+++ source/tools.adminer/stable/oswtools/tools.adminer.stable/adminer.php
@@ -50,7 +50,7 @@ function adminer_object() {
 	return new AdminerPlugin($plugins);
 }
 
-include abs_path.'resources/php/adminer/adminer-4.7.9.php';
+include abs_path.'resources/php/adminer/adminer-4.8.0.php';
 die();
 
 ?>
