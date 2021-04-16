b72304ab09d586cbf35943793136791a4f1750fa
Juergen Schwind <info@jbs-newmedia.de>
2021-04-16 14:50:21 +0200

    Komplett neue Version / komplettes rewrite

---
+++ source/tools.projectverify/stable/oswtools/frame/namespaces/osWFrame/Tools/Tool/ProjectVerify.php
@@ -0,0 +1,380 @@
+<?php
+
+/**
+ * This file is part of the osWFrame package
+ *
+ * @author Juergen Schwind
+ * @copyright Copyright (c) JBS New Media GmbH - Juergen Schwind (https://jbs-newmedia.com)
+ * @package osWFrame
+ * @link https://oswframe.com
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3
+ */
+
+namespace osWFrame\Tools\Tool;
+
+use osWFrame\Core as Frame;
+use osWFrame\Tools as Tools;
+
+class ProjectVerify extends CoreTool {
+
+	use Frame\BaseStaticTrait;
+	use Tools\BaseSettingsTrait;
+
+	/**
+	 * Major-Version der Klasse.
+	 */
+	private const CLASS_MAJOR_VERSION=1;
+
+	/**
+	 * Minor-Version der Klasse.
+	 */
+	private const CLASS_MINOR_VERSION=0;
+
+	/**
+	 * Release-Version der Klasse.
+	 */
+	private const CLASS_RELEASE_VERSION=0;
+
+	/**
+	 * Extra-Version der Klasse.
+	 * Zum Beispiel alpha, beta, rc1, rc2 ...
+	 */
+	private const CLASS_EXTRA_VERSION='beta';
+
+	/**
+	 * @var array
+	 */
+	private array $ignore_files=[];
+
+	/**
+	 * @var array
+	 */
+	private array $ignore_dirs=[];
+
+	/**
+	 * @var array
+	 */
+	private array $list=[];
+
+	/**
+	 * @var array
+	 */
+	private array $scanlist=[];
+
+	/**
+	 * @var array
+	 */
+	private array $readlist=[];
+
+	/**
+	 * ProjectVerify constructor.
+	 *
+	 * @param string $serverlist
+	 * @param string $package
+	 * @param string $release
+	 */
+	public function __construct(string $serverlist, string $package, string $release) {
+		parent::__construct($serverlist, $package, $release);
+		$this->loadSettings();
+	}
+
+	/**
+	 * @return object
+	 */
+	private function setIgnoreDefaultList():object {
+		$this->settings['projectverify_dirs'][]='oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'configure'.DIRECTORY_SEPARATOR.'top'.DIRECTORY_SEPARATOR;
+		$this->settings['projectverify_dirs'][]='oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'configure'.DIRECTORY_SEPARATOR.'topmiddle'.DIRECTORY_SEPARATOR;
+		$this->settings['projectverify_dirs'][]='oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'configure'.DIRECTORY_SEPARATOR.'middle'.DIRECTORY_SEPARATOR;
+		$this->settings['projectverify_dirs'][]='oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'configure'.DIRECTORY_SEPARATOR.'middlebottom'.DIRECTORY_SEPARATOR;
+		$this->settings['projectverify_dirs'][]='oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'configure'.DIRECTORY_SEPARATOR.'bottom'.DIRECTORY_SEPARATOR;
+		$this->settings['projectverify_dirs'][]='oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'filelist'.DIRECTORY_SEPARATOR;
+		$this->settings['projectverify_dirs'][]='oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'packagelist'.DIRECTORY_SEPARATOR;
+		$this->settings['projectverify_dirs'][]='oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'serverlist'.DIRECTORY_SEPARATOR;
+		$this->settings['projectverify_dirs'][]='oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'settings'.DIRECTORY_SEPARATOR;
+		$this->settings['projectverify_files'][]='frame'.DIRECTORY_SEPARATOR.'configure.php';
+		$this->settings['projectverify_files'][]='modules'.DIRECTORY_SEPARATOR.'configure.project.php';
+
+		return $this;
+	}
+
+	/**
+	 * @return object
+	 */
+	private function createList():object {
+		$this->setIgnoreDefaultList();
+		$this->readList();
+
+		$path=\osWFrame\Core\Settings::getStringVar('settings_framepath').'oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'filelist'.DIRECTORY_SEPARATOR;
+		foreach (glob($path.'*.json') as $file_list) {
+			$file_list=json_decode(file_get_contents($file_list), true);
+			if ((count($file_list)>0)&&(!isset($file_list[0]))) {
+				foreach ($file_list as $_file=>$checksum) {
+					if (substr(basename($_file), 0, 1)!='.') {
+						if ($checksum=='') {
+							$go=true;
+							foreach ($this->settings['projectverify_dirs'] as $idir) {
+								if ((DIRECTORY_SEPARATOR.$idir==substr($_file, 0, strlen(DIRECTORY_SEPARATOR.$idir)))||(DIRECTORY_SEPARATOR.$idir==substr($_file.DIRECTORY_SEPARATOR, 0, strlen($idir)+1))) {
+									$go=false;
+								}
+							}
+							if ($go===true) {
+								$this->readlist[substr($_file, 1)]=$checksum;
+							}
+						} else {
+							$go=true;
+							foreach ($this->settings['projectverify_dirs'] as $idir) {
+								if (DIRECTORY_SEPARATOR.$idir==substr($_file, 0, strlen(DIRECTORY_SEPARATOR.$idir))) {
+									$go=false;
+								}
+							}
+							foreach ($this->settings['projectverify_files'] as $ifile) {
+								if (DIRECTORY_SEPARATOR.$ifile==$_file) {
+									$go=false;
+								}
+							}
+							if ($go===true) {
+								$this->readlist[substr($_file, 1)]=$checksum;
+							}
+						}
+					}
+				}
+			}
+		}
+		ksort($this->readlist);
+
+		foreach ($this->scanlist as $element=>$checksum) {
+			if (isset($this->readlist[$element])) {
+				if ($this->readlist[$element]!=$checksum) {
+					if ($checksum=='') {
+						$this->list[$element]=['t'=>'d', 's'=>1];
+					} else {
+						$this->list[$element]=['t'=>'f', 's'=>1];
+					}
+				}
+				unset($this->scanlist[$element]);
+				unset($this->readlist[$element]);
+			} else {
+				if ($checksum=='') {
+					$this->list[$element]=['t'=>'d', 's'=>2];
+				} else {
+					$this->list[$element]=['t'=>'f', 's'=>2];
+				}
+			}
+		}
+
+		foreach ($this->readlist as $element=>$checksum) {
+			if ($checksum=='') {
+				$this->list[$element]=['t'=>'d', 's'=>3];
+			} else {
+				$this->list[$element]=['t'=>'f', 's'=>3];
+			}
+		}
+
+		ksort($this->list);
+
+		return $this;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function readList():object {
+		$this->setIgnoreDefaultList();
+		$dir=\osWFrame\Core\Settings::getStringVar('settings_framepath');
+		$this->scanlist=[];
+		if (Frame\Filesystem::isDir($dir)) {
+			$this->scanDirToArray($dir);
+			ksort($this->scanlist);
+
+			return $this;
+		}
+	}
+
+	/**
+	 * Engine zum Scannen von Verzeichnissen.
+	 *
+	 * @param string $dir
+	 * @return object
+	 */
+	private function scanDirToArray(string $dir):object {
+		$list=Frame\Filesystem::scanDir($dir);
+		if (!empty($list)) {
+			foreach ($list as $f) {
+				if (substr($f, 0, 1)!='.') {
+					if (Frame\Filesystem::isDir($dir.$f)) {
+						$_dir=str_replace(\osWFrame\Core\Settings::getStringVar('settings_framepath'), '', $dir.$f);
+						$go=true;
+						foreach ($this->settings['projectverify_dirs'] as $idir) {
+							if (($idir==$_dir)||($idir==$_dir.DIRECTORY_SEPARATOR)) {
+								$go=false;
+							}
+						}
+						if ($go===true) {
+							$this->scanlist[$_dir]='';
+							$this->scanDirToArray($dir.$f.DIRECTORY_SEPARATOR);
+						}
+					} else {
+						$_file=str_replace(\osWFrame\Core\Settings::getStringVar('settings_framepath'), '', $dir.$f);
+						$go=true;
+						foreach ($this->settings['projectverify_files'] as $ifile) {
+							if ($ifile==$_file) {
+								$go=false;
+							}
+						}
+						if ($go===true) {
+							$this->scanlist[$_file]=sha1_file($dir.$f);
+						}
+					}
+				}
+			}
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @param array $_projectverify_files
+	 * @param array $_projectverify_dirs
+	 * @param bool $load_settings
+	 * @return bool
+	 */
+	public function updateSettings(array $_projectverify_files, array $_projectverify_dirs, bool $load_settings=false):bool {
+		if ($load_settings===true) {
+			$projectverify_files=$this->getArraySetting('projectverify_files');
+		} else {
+			$projectverify_files=[];
+		}
+		foreach ($_projectverify_files as $value) {
+			$value=trim($value);
+			if (($value!='')&&(!in_array($value, $projectverify_files))) {
+				$projectverify_files[]=$value;
+			}
+		}
+
+		if ($load_settings===true) {
+			$projectverify_dirs=$this->getArraySetting('projectverify_dirs');
+		} else {
+			$projectverify_dirs=[];
+		}
+		foreach ($_projectverify_dirs as $value) {
+			$value=trim($value);
+			if (($value!='')&&(!in_array($value, $projectverify_dirs))) {
+				$projectverify_dirs[]=$value;
+			}
+		}
+
+		sort($projectverify_files);
+		$this->setArraySetting('projectverify_files', $projectverify_files);
+		sort($projectverify_dirs);
+		$this->setArraySetting('projectverify_dirs', $projectverify_dirs);
+		$this->writeSettings();
+
+		return true;
+	}
+
+	/**
+	 * @param string $element
+	 * @param string $type
+	 * @return bool
+	 */
+	public function addIgnore(string $element, string $type):bool {
+		if ($type=='f') {
+			return $this->updateSettings([$element], [], true);
+		}
+		if ($type=='d') {
+			return $this->updateSettings([], [$element.DIRECTORY_SEPARATOR], true);
+		}
+
+		return false;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getList():array {
+		$this->createList();
+
+		return $this->list;
+	}
+
+	/**
+	 *
+	 */
+	public function forceDownload():void {
+		$serverlist=[];
+		$path=\osWFrame\Core\Settings::getStringVar('settings_framepath').'oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'packagelist'.DIRECTORY_SEPARATOR;
+		foreach (glob($path.'*.json') as $package_list) {
+			$file_list=json_decode(file_get_contents($package_list), true);
+			foreach ($file_list as $package=>$element) {
+				$serverlist[$package]=substr(str_replace($path, '', $package_list), 0, -5);
+			}
+		}
+
+		$filelist=[];
+		$path=\osWFrame\Core\Settings::getStringVar('settings_framepath').'oswtools'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'json'.DIRECTORY_SEPARATOR.'filelist'.DIRECTORY_SEPARATOR;
+		foreach (glob($path.'*.json') as $file_list) {
+			$list=$file_list;
+			$file_list=json_decode(file_get_contents($file_list), true);
+			$filelist[$list]=$file_list;
+		}
+
+		$packagelist=[];
+		foreach ($filelist as $list=>$elements) {
+			foreach ($elements as $file=>$checksum) {
+				if ($checksum!='') {
+					$packagelist[substr($file, 1)]=substr(str_replace($path, '', $list), 0, -5);
+				}
+			}
+		}
+
+		$file='projectverify-'.date('YmdHis').'.zip';
+		$zfile=Frame\Settings::getStringVar('settings_abspath').Frame\Settings::getStringVar('cache_path').DIRECTORY_SEPARATOR.$file;
+		$Zip=new Frame\Zip($zfile);
+		if ($Zip->openFile(\ZIPARCHIVE::CREATE)===true) {
+			$del_list=[];
+			foreach ($this->getList() as $element=>$status) {
+				if ((isset($packagelist[$element]))&&(isset($serverlist[$packagelist[$element]]))) {
+					// change
+					if ($status['s']==1) {
+						$Zip->addEmptyDir($serverlist[$packagelist[$element]].DIRECTORY_SEPARATOR.'source'.DIRECTORY_SEPARATOR.str_replace('-', DIRECTORY_SEPARATOR, $packagelist[$element]).DIRECTORY_SEPARATOR.dirname($element));
+						if ($status['t']=='f') {
+							$Zip->addFile(Frame\Settings::getStringVar('settings_framepath').$element, $serverlist[$packagelist[$element]].DIRECTORY_SEPARATOR.'source'.DIRECTORY_SEPARATOR.str_replace('-', DIRECTORY_SEPARATOR, $packagelist[$element]).DIRECTORY_SEPARATOR.$element);
+						}
+					}
+					if ($status['s']==3) {
+						$del_list[$serverlist[$packagelist[$element]]][]='source'.DIRECTORY_SEPARATOR.str_replace('-', DIRECTORY_SEPARATOR, $packagelist[$element]).DIRECTORY_SEPARATOR.$element;
+					}
+				} else {
+					if ($status['s']==2) {
+						if ($status['t']=='d') {
+							$Zip->addEmptyDir('new_files'.DIRECTORY_SEPARATOR.$element.DIRECTORY_SEPARATOR);
+						}
+						if ($status['t']=='f') {
+							if (dirname($element)!='.') {
+								$Zip->addEmptyDir('new_files'.DIRECTORY_SEPARATOR.dirname($element).DIRECTORY_SEPARATOR);
+							}
+							$Zip->addFile(Frame\Settings::getStringVar('settings_framepath').$element, 'new_files'.DIRECTORY_SEPARATOR.$element);
+						}
+					}
+				}
+			}
+			foreach ($del_list as $list=>$files) {
+				if ($list!='') {
+					$Zip->addEmptyDir($list.DIRECTORY_SEPARATOR.'source'.DIRECTORY_SEPARATOR);
+					$Zip->addFromString($list.DIRECTORY_SEPARATOR.'source'.DIRECTORY_SEPARATOR.'deleted_files.list', implode("\n", $files));
+				}
+			}
+			$Zip->close();
+		}
+		header("Content-type: application/octet-stream");
+		header("Content-Disposition: attachment; filename=\"".$file."\"");
+		header("Content-Transfer-Encoding: binary");
+		header("Content-Length: ".filesize($zfile));
+		readfile($zfile);
+		Frame\Filesystem::delFile($zfile);
+		Frame\Settings::dieScript();
+	}
+
+}
+
+?>
---
+++ source/tools.projectverify/stable/oswtools/modules/tools.projectverify.stable/tpl/content.tpl.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * This file is part of the osWFrame package
+ *
+ * @author Juergen Schwind
+ * @copyright Copyright (c) JBS New Media GmbH - Juergen Schwind (https://jbs-newmedia.com)
+ * @package osWFrame
+ * @link https://oswframe.com
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3
+ */
+
+?>
+
+<?php if (in_array(\osWFrame\Core\Settings::getAction(), ['about'])): ?>
+
+	<?php include \osWFrame\Core\Settings::getStringVar('settings_abspath').'resources'.DIRECTORY_SEPARATOR.'tpl'.DIRECTORY_SEPARATOR.'about.tpl.php'; ?>
+
+<?php elseif (in_array(\osWFrame\Core\Settings::getAction(), ['changelog'])): ?>
+
+	<?php include \osWFrame\Core\Settings::getStringVar('settings_abspath').'resources'.DIRECTORY_SEPARATOR.'tpl'.DIRECTORY_SEPARATOR.'changelog.tpl.php'; ?>
+
+<?php elseif (in_array(\osWFrame\Core\Settings::getAction(), ['settings'])): ?>
+
+	<?php echo $osW_Form->startForm('oswtools_projectverify_form', 'current', '', ['input_addid'=>true]); ?>
+
+	<label class="font-weight-bold" for="projectverify_dirs">Ignored directories:</label>
+	<div class="input-group mb-3">
+		<div class="input-group-prepend">
+			<span class="input-group-text"><i class="fas fa-folder fa-fw"></i></span>
+		</div>
+		<?php echo $osW_Form->drawTextareaField('projectverify_dirs', implode("\n", $Tool->getArraySetting('projectverify_dirs')), ['input_class'=>'form-control', 'input_errorclass'=>'is-invalid', 'input_parameter'=>'rows="8"']) ?>
+		<div class="invalid-feedback"><?php echo $osW_Form->getErrorMessage('projectverify_dirs') ?></div>
+	</div>
+
+	<label class="font-weight-bold" for="projectverify_files">Ignored files:</label>
+	<div class="input-group mb-3">
+		<div class="input-group-prepend">
+			<span class="input-group-text"><i class="fas fa-file fa-fw"></i></span>
+		</div>
+		<?php echo $osW_Form->drawTextareaField('projectverify_files', implode("\n", $Tool->getArraySetting('projectverify_files')), ['input_class'=>'form-control', 'input_errorclass'=>'is-invalid', 'input_parameter'=>'rows="8"']) ?>
+		<div class="invalid-feedback"><?php echo $osW_Form->getErrorMessage('projectverify_files') ?></div>
+	</div>
+
+	<hr/>
+
+	<a href="javascript:$('#oswtools_projectverify_form').submit()" class="btn btn-primary d-block">Save settings</a>
+
+	<?php echo $osW_Form->drawHiddenField('doaction', 'dosave'); ?>
+
+	<?php echo $osW_Form->endForm(); ?>
+
+<?php else: ?>
+
+	<table id="oswtools_projectverify" class="table table-striped table-bordered">
+		<thead>
+		<tr>
+			<th class="text-center pt-1">Status</th>
+			<th class="text-center mt-1">Type</th>
+			<th>Directory/File</th>
+			<th class="text-center">Options</th>
+		</tr>
+		</thead>
+		<tbody>
+
+		<?php if ($Tool->getList()!=[]): ?>
+
+			<?php foreach ($Tool->getList() as $element=>$status): ?>
+				<tr>
+					<td class="text-center"><span class="btn btn-xs" disabled><?php if ($status['s']==1): ?><i class="fas fa-not-equal fa-fw"></i><?php elseif ($status['s']==2): ?><i class="fas fa-plus fa-fw"></i><?php elseif ($status['s']==3): ?><i class="fas fa-minus fa-fw"></i><?php else: ?><i class="fas fa-bug"></i><?php endif ?></span></td>
+					<td class="text-center"><span class="btn btn-xs" disabled><?php if ($status['t']=='f'): ?><i class="fas fa-file fa-fw"></i><?php elseif ($status['t']='d'): ?><i class="fas fa-folder fa-fw"></i><?php else: ?><i class="fas fa-bug"></i><?php endif ?></span></td>
+					<td><?php echo $element ?></td>
+					<td class="projectverify_options text-center"><a title="Remove" href="javascript:engine('<?php echo $this->buildhrefLink('current', 'action=settings&doaction=doignore')?>', '<?php echo $this->buildhrefLink('current', 'action=start')?>', '<?php echo $element?>', '<?php echo $status['t']?>')" class="btn btn-primary btn-xs"><i class="fa fa-times fa-fw"></i></a></td>
+				</tr>
+			<?php endforeach ?>
+
+		<?php endif ?>
+
+		</tbody>
+	</table>
+
+	<?php if ($Tool->getList()!=[]): ?>
+
+		<hr/>
+
+		<a href="<?php echo $this->buildhrefLink('current', 'action=start&doaction=download')?>" class="btn btn-primary d-block">Create and download ZIP-archive</a>
+
+	<?php endif ?>
+
+<?php endif ?>
