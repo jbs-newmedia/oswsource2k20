d8ddd560e84196179486169e0b3331a0466e612f
Juergen Schwind <info@jbs-newmedia.de>
2021-11-04 22:51:44 +0100

    Fehler im Modul "ajaxfunc" behoben

--- source/oswframe.ddm4/stable/frame/ddm4/datatableajax/yesno/php/content.inc.php
+++ source/oswframe.ddm4/stable/frame/ddm4/datatableajax/yesno/php/content.inc.php
@@ -13,7 +13,7 @@
 $datatableajax_rule='($("input[name=\''.$values['rule_key'].'\']:checked").val()=='.implode(' || $("input[name=\''.$values['rule_key'].'\']:checked").val()==', $values['rule_value']).')';
 $this->setElementStorage('datatableajax_rule', $datatableajax_rule);
 
-$datatableajax_selected='$("input[name=\''.$values['rule_key'].'\']").change(function(){ddm4formular_'.$ddm_group.'();});';
+$datatableajax_selected='$("input[name=\''.$values['rule_key'].'\']").change(function(){ddm4formular_'.$this->getName().'();});';
 $this->setElementStorage('datatableajax_selected', $datatableajax_selected);
 
 ?>
