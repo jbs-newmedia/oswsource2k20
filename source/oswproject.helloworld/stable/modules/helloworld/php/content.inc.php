f13fd24d95ad753ceb135c185641a18c5fc41c3f
Juergen Schwind <info@jbs-newmedia.de>
2022-08-06 12:29:41 +0200

    HelloWorld aktualisiert

--- source/oswproject.helloworld/stable/modules/helloworld/php/content.inc.php
+++ source/oswproject.helloworld/stable/modules/helloworld/php/content.inc.php
@@ -1,15 +1,13 @@
 <?php
 
+/**
+ * @var \osWFrame\Core\Template $osW_Template
+ */
+
 $foo='Hello World @ '.date('Y/m/d H/i/s');
 
-#$osW_TemplateLoader = new osWFrame\Core\TemplateLoader();
-#$osW_TemplateLoader->addTemplateCSSFile('head', 'data/resources/content/layout.css');
-#$osW_TemplateLoader->addTemplate($osW_Template);
-
+$osW_Template->addStringTag('title', 'Hello World');
 
 $osW_Template->setVarAsCopy('bar', $foo);
-
-
-$osW_Template->buildhrefLink('test', 'a=1&b=2&c=3#Test');
 
 ?>
