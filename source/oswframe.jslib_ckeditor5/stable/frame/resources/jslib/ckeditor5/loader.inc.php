9444f94b481b45b2edb7e045f4ab4eec0011e2ae
jschwind <info@juergen-schwind.de>
2022-10-15 09:53:36 +0200

    Cachen der Resource nun möglich

--- source/oswframe.jslib_ckeditor5/stable/frame/resources/jslib/ckeditor5/loader.inc.php
+++ source/oswframe.jslib_ckeditor5/stable/frame/resources/jslib/ckeditor5/loader.inc.php
@@ -27,7 +27,7 @@ $dir=strtolower($this->getClassName().DIRECTORY_SEPARATOR.$lib_name);
 $name=$lib_name.DIRECTORY_SEPARATOR.$version.'.resource';
 if (Resource::existsResource($this->getClassName(), $name)!==true) {
 	Resource::copyResourcePath('frame'.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'jslib'.DIRECTORY_SEPARATOR.$lib_name.DIRECTORY_SEPARATOR.$version.DIRECTORY_SEPARATOR.'js'.DIRECTORY_SEPARATOR, $dir.DIRECTORY_SEPARATOR.$version.DIRECTORY_SEPARATOR.'js'.DIRECTORY_SEPARATOR);
-	#Resource::writeResource($this->getClassName(), $name, time());
+	Resource::writeResource($this->getClassName(), $name, time());
 }
 
 $path=Resource::getRelDir().$dir.DIRECTORY_SEPARATOR.$version.DIRECTORY_SEPARATOR;
