d6bdf136599d7fec254303ad1388b3797ba994e8
jschwind <info@juergen-schwind.de>
2022-11-28 21:09:31 +0100

    Überladen vom Modul ermöglicht

--- source/oswframe.favicon/stable/modules/_favicon/php/header.inc.php
+++ source/oswframe.favicon/stable/modules/_favicon/php/header.inc.php
@@ -10,8 +10,17 @@
  * @license MIT License
  */
 
-\osWFrame\Core\Settings::setStringVar('frame_default_engine', 'favicon');
-\osWFrame\Core\Settings::setStringVar('frame_default_output', 'favicon');
-\osWFrame\Core\Settings::setBoolVar('session_enabled', false);
+if (\osWFrame\Core\Settings::getStringVar('favicon_module')==\osWFrame\Core\Settings::getStringVar('frame_default_module')) {
+	\osWFrame\Core\Settings::setStringVar('frame_default_engine', 'favicon');
+	\osWFrame\Core\Settings::setStringVar('frame_default_output', 'favicon');
+	\osWFrame\Core\Settings::setBoolVar('session_enabled', false);
+} else {
+	\osWFrame\Core\Settings::setStringVar('project_default_module', \osWFrame\Core\Settings::getStringVar('favicon_module'));
+	\osWFrame\Core\Settings::setStringVar('frame_default_module', \osWFrame\Core\Settings::getStringVar('favicon_module'));
+	$file=\osWFrame\Core\Settings::getStringVar('settings_abspath').'modules'.DIRECTORY_SEPARATOR.\osWFrame\Core\Settings::getStringVar('project_default_module').DIRECTORY_SEPARATOR.'php'.DIRECTORY_SEPARATOR.'header.inc.php';
+	if (file_exists($file)) {
+		require_once $file;
+	}
+}
 
 ?>
