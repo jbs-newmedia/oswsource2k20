ebe5848af495a749d4578dd4e7fc399bf11bed48
jschwind <info@juergen-schwind.de>
2023-07-07 12:09:27 +0200

    "outputSVG" wird jetzt über Optionen gesteuert

--- source/oswframe.qrcodestatic/stable/frame/namespaces/osWFrame/Core/QRCodeStatic.php
+++ source/oswframe.qrcodestatic/stable/frame/namespaces/osWFrame/Core/QRCodeStatic.php
@@ -27,7 +27,7 @@ class QRCodeStatic {
 	/**
 	 * Minor-Version der Klasse.
 	 */
-	private const CLASS_MINOR_VERSION=0;
+	private const CLASS_MINOR_VERSION=1;
 
 	/**
 	 * Release-Version der Klasse.
@@ -52,33 +52,72 @@ class QRCodeStatic {
 
 	/**
 	 * @param string $data
+	 * @param array $option
 	 * @return void
 	 */
-	public static function outputSVG(string $data) {
+	public static function outputSVG(string $data, array $option=[]) {
 		$gzip=true;
 
-		$options=new QROptions(['version'=>4, 'outputType'=>QRCode::OUTPUT_MARKUP_SVG, 'imageBase64'=>false, 'eccLevel'=>QRCode::ECC_L, 'addQuietzone'=>true, 'cssClass'=>'my-css-class', 'svgOpacity'=>1.0, 'svgDefs'=>'
-		<linearGradient id="g2">
-			<stop offset="0%" stop-color="'.Settings::getStringVar('project_theme_color').'" />
-			<stop offset="100%" stop-color="'.Settings::getStringVar('project_theme_color').'" />
-		</linearGradient>
-		<linearGradient id="g1">
-			<stop offset="0%" stop-color="'.Settings::getStringVar('project_theme_color').'" />
-			<stop offset="100%" stop-color="'.Settings::getStringVar('project_theme_color').'" />
-		</linearGradient>
-		<style>rect{shape-rendering:crispEdges}</style>', 'moduleValues'=>[// finder
-			1536=>'url(#g1)', // dark (true)
-			6=>'#fff', // light (false)
-			// alignment
-			2560=>'url(#g1)', 10=>'#fff', // timing
-			3072=>'url(#g1)', 12=>'#fff', // format
-			3584=>'url(#g1)', 14=>'#fff', // version
-			4096=>'url(#g1)', 16=>'#fff', // data
-			1024=>'url(#g2)', 4=>'#fff', // darkmodule
-			512=>'url(#g1)', // separator
-			8=>'#fff', // quietzone
-			18=>'#fff',],]);
+		if (!isset($option['version'])) {
+			$option['version']=4;
+		}
 
+		if (!isset($option['outputType'])) {
+			$option['outputType']=QRCode::OUTPUT_MARKUP_SVG;
+		}
+
+		if (!isset($option['imageBase64'])) {
+			$option['imageBase64']=false;
+		}
+
+		if (!isset($option['eccLevel'])) {
+			$option['eccLevel']=QRCode::ECC_L;
+		}
+
+		if (!isset($option['addQuietzone'])) {
+			$option['addQuietzone']=true;
+		}
+
+		if (!isset($option['cssClass'])) {
+			$option['cssClass']='my-css-class';
+		}
+
+		if (!isset($option['svgOpacity'])) {
+			$option['svgOpacity']=1.0;
+		}
+
+		if (!isset($option['project_theme_color_start'])) {
+			$option['project_theme_color_start']='#000000';
+		}
+
+		if (!isset($option['project_theme_color_end'])) {
+			$option['project_theme_color_end']='#000000';
+		}
+
+		if (!isset($option['project_theme_color_bg'])) {
+			$option['project_theme_color_bg']='#ffffff';
+		}
+
+		if (!isset($option['svgDefs'])) {
+			$option['svgDefs']='<linearGradient id="g2"><stop offset="0%" stop-color="'.$option['project_theme_color_start'].'" /><stop offset="100%" stop-color="'.$option['project_theme_color_end'].'" /></linearGradient><linearGradient id="g1"><stop offset="0%" stop-color="'.$option['project_theme_color_start'].'" /><stop offset="100%" stop-color="'.$option['project_theme_color_end'].'" /></linearGradient><style>rect{shape-rendering:crispEdges}</style>';
+		}
+
+		if (!isset($option['moduleValues'])) {
+			$option['moduleValues']=[// finder
+				1536=>'url(#g1)', // dark (true)
+				6=>$option['project_theme_color_bg'], // light (false)
+				// alignment
+				2560=>'url(#g1)', 10=>$option['project_theme_color_bg'], // timing
+				3072=>'url(#g1)', 12=>$option['project_theme_color_bg'], // format
+				3584=>'url(#g1)', 14=>$option['project_theme_color_bg'], // version
+				4096=>'url(#g1)', 16=>$option['project_theme_color_bg'], // data
+				1024=>'url(#g2)', 4=>$option['project_theme_color_bg'], // darkmodule
+				512=>'url(#g1)', // separator
+				8=>$option['project_theme_color_bg'], // quietzone
+				18=>$option['project_theme_color_bg']];
+		}
+
+		$options=new QROptions(['version'=>$option['version'], 'outputType'=>$option['outputType'], 'imageBase64'=>$option['imageBase64'], 'eccLevel'=>$option['eccLevel'], 'addQuietzone'=>$option['addQuietzone'], 'cssClass'=>$option['cssClass'], 'svgOpacity'=>$option['svgOpacity'], 'svgDefs'=>$option['svgDefs'], 'moduleValues'=>$option['moduleValues']]);
 		$qrcode=(new QRCode($options))->render($data);
 
 		header('Content-type: image/svg+xml');
