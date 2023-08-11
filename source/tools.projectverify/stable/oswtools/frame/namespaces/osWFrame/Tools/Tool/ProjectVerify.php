b6df1c94427ebd4017bf297744185d32caa06de0
jschwind <info@juergen-schwind.de>
2023-08-11 18:05:34 +0200

    account.email hinzugefügt

--- source/tools.projectverify/stable/oswtools/frame/namespaces/osWFrame/Tools/Tool/ProjectVerify.php
+++ source/tools.projectverify/stable/oswtools/frame/namespaces/osWFrame/Tools/Tool/ProjectVerify.php
@@ -83,6 +83,7 @@ class ProjectVerify extends CoreTool {
 		$this->settings['projectverify_files'][]='frame'.DIRECTORY_SEPARATOR.'configure.php';
 		$this->settings['projectverify_files'][]='modules'.DIRECTORY_SEPARATOR.'configure.project.php';
 		$this->settings['projectverify_files'][]='oswtools'.DIRECTORY_SEPARATOR.'frame.key';
+		$this->settings['projectverify_files'][]='oswtools'.DIRECTORY_SEPARATOR.'account.email';
 
 		return $this;
 	}
